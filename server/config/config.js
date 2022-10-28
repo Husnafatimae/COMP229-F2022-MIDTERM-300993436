bc96ae6b11ebb990baae20873aa886efcfe8f857
Husna Fatima <hfatima4@my.centennialcollege.ca>
2022-10-28 18:51:54 -0400

    added mongodbatlas

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,3 @@
-export const MongoURI = "mongodb://localhost:27017/media";
 export const Secret = "someSecret";
+//export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv//husna:husna12345@comp229-midterm-3009934.dfjflzh.mongodb.net/media1?retryWrites=true&w=majority";
